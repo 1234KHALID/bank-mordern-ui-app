1593d624748f13d451ca28e9dd229edb63e3c5fb
Mohd Khalid <mohammad@prodios.com>
2025-07-27 01:03:45 +0530

    create navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import styles from './styles';
+import { Navbar } from './components';
+
 function App() {
   return (
-    <div className="bg-primary w-full overflow-hidden">
-      <div></div>
+    <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>{/* hero */}</div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>{/* stats */}</div>
+      </div>
     </div>
   );
 }
